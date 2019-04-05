602d17c6edd44beccc02e36235aefe4b5a9c0784
Nick Tyler <tylern@canisius.edu>
2019-04-05 12:18:53 -0400

    Make root kernel work

--- python3/.rootnb/jupyter_notebook_config.py
+++
@@ -1,7 +0,0 @@
-import os
-rootbin = '/usr/local/root/bin'
-rootlib = '/usr/local/root/lib'
-os.environ['PYTHONPATH']      = '%s' % rootlib + ':' + os.getenv('PYTHONPATH', '')
-os.environ['PATH']            = '%s:%s/bin' % (rootbin,rootbin) + ':' + os.getenv('PATH', '')
-os.environ['LD_LIBRARY_PATH'] = '%s' % rootlib + ':' + os.getenv('LD_LIBRARY_PATH', '')
-c.NotebookApp.ip = '*'
